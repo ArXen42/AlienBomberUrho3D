6c21d12ff64d7e1150e7c3388b0ce94507f20e24
Georgii Pelageikin <arxen42@tutanota.com>
2019-02-22 03:39:57 +0300

    Fixed incorrect signature change

--- AlienBomber/Source/CollisionDetection/CollisionsAggregator.hpp
+++ AlienBomber/Source/CollisionDetection/CollisionsAggregator.hpp
@@ -36,12 +36,12 @@ private:
 
 	void OnContact(CollisionType type, VariantMap& eventData);
 
-	void OnPhysicsBeginContact2D(const StringHash& eventType, VariantMap& eventData)
+	void OnPhysicsBeginContact2D(StringHash eventType, VariantMap& eventData)
 	{
 		OnContact(CollisionType::BeginContact, eventData);
 	}
 
-	void OnPhysicsEndContact2D(const StringHash& eventType, VariantMap& eventData)
+	void OnPhysicsEndContact2D(StringHash eventType, VariantMap& eventData)
 	{
 		OnContact(CollisionType::EndContact, eventData);
 	}
